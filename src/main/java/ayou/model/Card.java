97539eb9a4ce684ba4e170f1d759e4c9b6153a0f
Jérémy <friendlycube@gmail.com>
2016-06-08 14:20:33 +0200

    Update Constructor !

--- src/main/java/ayou/model/Card.java
+++ src/main/java/ayou/model/Card.java
@@ -1,4 +1,5 @@
 package ayou.model;
+
 /**
  * @author aronowij
  *
@@ -8,13 +9,13 @@ public class Card {
 	private final int ID_CARD;
 	private String name;
 	private final String ID_IMG;
-	
+
 	private final int POWER;
 	private int powerBuffs;
 	private final int MAX_HIT_POINTS;
 	private int hitPoints;
 	private int lifeBuffs;
-	
+
 	private boolean canAttack;
 	private boolean celerity;
 	private int healAlly;
@@ -23,31 +24,33 @@ public class Card {
 	private boolean buffAllAllies;
 	private int debuffEnemy;
 	private boolean debuffAllEnemies;
-	
 
-	public Card(int power, int hitPoints, String name) {
+	public Card(int idCard, String name, String idImg, int power, int powerBuff, int maxHitPoints, int hitPoints,
+			int lifeBuffs, boolean canAtk, boolean celerity, int healAlly, boolean healAllAllies, 
+			int buffAlly, boolean buffAllAllies, int debuffEnemy, boolean debuffAllEnemies) {
+		this.ID_CARD=idCard;
+		this.name = name;
+		this.ID_IMG="test";
 		this.POWER = power;
 		this.MAX_HIT_POINTS = hitPoints;
 		this.hitPoints = hitPoints;
 		this.name = name;
-		ID_CARD=0;
-		ID_IMG="test";
-		powerBuffs = 0;
-		lifeBuffs = 0;
-		canAttack=true;
-		celerity=false;
-		healAlly=0;
-		healAllAllies=false;
-		buffAlly=0;
-		buffAllAllies=false;
-		debuffEnemy=0;
-		debuffAllEnemies=false;
+		this.powerBuffs = powerBuff;
+		this.lifeBuffs = lifeBuffs;
+		this.canAttack=canAtk;
+		this.celerity=celerity;
+		this.healAlly=healAlly;
+		this.healAllAllies=healAllAllies;
+		this.buffAlly=buffAlly;
+		this.buffAllAllies=buffAllAllies;
+		this.debuffEnemy=debuffEnemy;
+		this.debuffAllEnemies=debuffAllEnemies;
 	}
 
 	public void attack(Card targetCard) {
-		if(canAttack){
+		if (canAttack) {
 			targetCard.setHitPoints(getPower());
-			setHitPoints(targetCard.getPower());	
+			setHitPoints(targetCard.getPower());
 		}
 	}
 
@@ -58,50 +61,49 @@ public class Card {
 	public int getMaxHitPoints() {
 		return MAX_HIT_POINTS + lifeBuffs;
 	}
-	
+
 	public int getHitPoints() {
 		return hitPoints;
 	}
-	
+
 	public void setHitPoints(int damages) {
 		hitPoints -= damages;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	@Override
 	public String toString() {
 		return name + " : " + getPower() + " - " + hitPoints;
 	}
-	
-	public void getHealed(int heal){
-		if(this.hitPoints+heal<this.MAX_HIT_POINTS){
-			hitPoints+=heal;
-		}
-		else{
-			hitPoints=MAX_HIT_POINTS;
+
+	public void getHealed(int heal) {
+		if (this.hitPoints + heal < this.MAX_HIT_POINTS) {
+			hitPoints += heal;
+		} else {
+			hitPoints = MAX_HIT_POINTS;
 		}
 	}
 
 	public void invocation(Board boardAlly) {
 		// TODO Auto-generated method stub
-		if(healAlly>0){
+		if (healAlly > 0) {
 			if (healAllAllies) {
-				for(Card card: boardAlly.getCards()){
+				for (Card card : boardAlly.getCards()) {
 					card.getHealed(this.healAlly);
 				}
-			}
-			else {
-				
-			}
-		}if (debuffEnemy>0) {
-			if (debuffAllEnemies) {
-				
+			} else {
+
 			}
 		}
-		
+		if (debuffEnemy > 0) {
+			if (debuffAllEnemies) {
+
+			}
+		}
+
 	}
 
 }
