a0ad44cbc75d0d1a11109071b5c617a6ff4dea25
Jeremie ARONOWIEZ <jeremie.aronowiez@etudiant.univ-lille1.fr>
2016-06-08 14:56:52 +0200

    Merge

--- src/main/java/ayou/main/Main.java
+++ src/main/java/ayou/main/Main.java
@@ -1,25 +1,22 @@
 package ayou.main;
 
 import ayou.model.GameLoop;
-import ayou.view.Window;
+import ayou.view.Viewer;
 
 public class Main {
-	public static Window window;
 
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				//window=new Window(1300, 800, "Awesome");
-				new GameLoop();
-				
+				new Viewer();
 			}
 		});
-		
+
 	}
-	
-	public static void run(){
+
+	public static void run() {
 		new GameLoop();
 	}
 
