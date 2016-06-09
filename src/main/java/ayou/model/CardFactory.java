fd5e965c634554a55d7c515f82a6c1351fa6fbb7
Lucas DUTRIEUX <dutrieul@ayou04.iut-infobio.priv.univ-lille1.fr>
2016-06-09 14:40:25 +0200

    sprint 7.7 LUCAS Y ROXX DU PONEY JEU FONCTIONNEL

---
+++ src/main/java/ayou/model/CardFactory.java
@@ -0,0 +1,21 @@
+package ayou.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CardFactory {
+
+	public static List<Card> genereDeck(int size) {
+		List<Card> cardList = new ArrayList<>(size);
+		
+		cardList.add(new Card(1, "Kinan",null,5,15, true,false,0,false,0,false,0,false,1));
+		cardList.add(new Card(2, "Torren", null,4, 10, true, false,0,false,0,false,0,false,0));
+		
+		
+		
+		//cardList.add(new Card(idCard, name, idImg, power, maxHitPoints, canAtk, celerity, healAlly, healAllAllies, buffAlly, buffAllAllies, debuffEnemy, debuffAllEnemies, cout)
+		return cardList;
+	}
+
+}
