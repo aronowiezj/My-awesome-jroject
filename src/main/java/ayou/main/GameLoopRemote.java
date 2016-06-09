cf94b04d1e51f8d6563966402cd47ca3a979be02
Lucas DUTRIEUX <dutrieul@ayou04.iut-infobio.priv.univ-lille1.fr>
2016-06-09 15:02:05 +0200

    sprint 7.8 JEU FONCTIONNEL

--- src/main/java/ayou/main/GameLoopRemote.java
+++ src/main/java/ayou/main/GameLoopRemote.java
@@ -10,19 +10,8 @@ import ayou.model.Player;
 public class GameLoopRemote {
 	
 	public static void main(String[] args) {
-		int i = 0;
 		while (true) {
 			/////
-			i++;
-			Entity<Player> userEntity = Entity.entity(new Player("Papynou"+i), MediaType.APPLICATION_JSON);
-			
-			Player p = ClientBuilder.newClient()
-					.target("http://localhost:8080/v1/player")
-					.request()
-					.post(userEntity)
-					.readEntity(Player.class);
-			
-			System.out.println(p.getName());
 			//////
 			
 			CardEntity card = ClientBuilder.newClient().target("http://localhost:8080/v1/card/5").request().get(CardEntity.class);
