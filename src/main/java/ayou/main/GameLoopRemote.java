b8bd79a0e467123d483614c8570a839fe2094ebc
FunKyWizzarD <anardroide@live.fr>
2016-06-09 10:40:04 +0200

    + Reseau INCOMING - {HOPE}

--- src/main/java/ayou/main/GameLoopRemote.java
+++ src/main/java/ayou/main/GameLoopRemote.java
@@ -1,7 +1,10 @@
 package ayou.main;
 
+import java.util.List;
+
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 
 import ayou.model.Player;
@@ -9,8 +12,11 @@ import ayou.model.Player;
 public class GameLoopRemote {
 	
 	public static void main(String[] args) {
+		int i = 0;
 		while (true) {
-			Entity<Player> userEntity = Entity.entity(new Player("OreWa"), MediaType.APPLICATION_JSON);
+			/////
+			i++;
+			Entity<Player> userEntity = Entity.entity(new Player("Papynou"+i), MediaType.APPLICATION_JSON);
 			
 			Player p = ClientBuilder.newClient()
 					.target("http://localhost:8080/v1/player")
@@ -19,9 +25,11 @@ public class GameLoopRemote {
 					.readEntity(Player.class);
 			
 			System.out.println(p.getName());
+			//////
 			
-			Player player = ClientBuilder.newClient().target("http://localhost:8080/v1/player").request().get(Player.class);
-			System.out.print(player.toString());
+			List<Player> player = ClientBuilder.newClient().target("http://localhost:8080/v1/player").request().get(new GenericType<List<Player>>(){});
+			System.out.println(player);
+			
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
